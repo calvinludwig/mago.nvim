7fef5b25f09c011f64138f91d01627e51037297e
Braxton Brown <braxton.brown@outlook.com>
2026-02-26 12:02:12 -0700

    add logging configuration

--- lua/mago-nvim/init.lua
+++ lua/mago-nvim/init.lua
@@ -1,7 +1,9 @@
 local M = {}
 
-function M.setup()
+function M.setup(opts)
+  opts = opts or {}
   local exe = require 'mago-nvim.executable'
+  exe.setup(opts)
   if not exe.init() then
     vim.notify('[mago.nvim] Mago executable not found', vim.log.levels.ERROR)
     return
